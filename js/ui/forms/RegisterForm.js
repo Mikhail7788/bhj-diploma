42837beff7543c16324987565ccee8ec70dbf726
Mikhail7788 <mikhail843@gmail.com>
2020-05-03 20:07:18 +0300

    добавлена проверка ответа сервера

--- js/ui/forms/RegisterForm.js
+++ js/ui/forms/RegisterForm.js
@@ -13,9 +13,14 @@ class RegisterForm extends AsyncForm {
   onSubmit(options) {
       
     User.register(options.data, (err, response) => {
-    this.element.reset();
-    App.setState('user-logged');
-    App.getModal('register').close();
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('register').close();
+      } else {
+          console.log(err);
+      }
+      
     });
   }
 }
